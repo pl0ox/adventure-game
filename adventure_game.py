2adcb1b9b85b057d0bf79e12c403aa08d155933d
pl0ox <84785786+pl0ox@users.noreply.github.com>
2023-06-06 17:55:36 +0300

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -14,18 +14,86 @@ def intro():
 def explore_forest(total_score):
     print_pause("You start exploring the forest.")
     print_pause("As you walk, you hear strange sounds and see peculiar creatures.")
-    print_pause("Suddenly, you stumble upon an old tree with a hidden door.")
-    print_pause("Do you want to open the door? (1) Yes (2) No")
-    choice = input()
-    if choice == '1':
-        print_pause("You open the door and find a chest filled with gold and jewels!")
-        print_pause("Congratulations! You have found the hidden treasure. You are victorious!")
-        total_score += 10
-    elif choice == '2':
-        print_pause("You decide not to open the door and continue exploring.")
+    
+    main_scenario = random.choice([1, 2, 3])
+    
+    if main_scenario == 1:
+        print_pause("While wandering through the forest, you stumble upon a hidden treasure chest.")
+        print_pause("Do you want to open the chest? (1) Yes (2) No")
+        choice = input()
+        if choice == '1':
+            outcome = random.choice([1, 2, 3])
+            if outcome == 1:
+                print_pause("You open the chest and find a stash of gold coins!")
+                print_pause("Congratulations! You have found treasure. Your score increases!")
+                total_score += 10
+            elif outcome == 2:
+                print_pause("As you open the chest, a cloud of poisonous gas emerges.")
+                print_pause("You quickly retreat, avoiding the danger.")
+                print_pause("Your score remains unchanged.")
+            else:
+                print_pause("Upon opening the chest, a mischievous forest spirit appears.")
+                print_pause("The spirit grants you a small magical trinket.")
+                print_pause("Your score slightly increases.")
+                total_score += 3
+        elif choice == '2':
+            print_pause("You decide not to open the chest and continue exploring.")
+        else:
+            print_pause("Invalid input. Please enter 1 or 2.")
+            return explore_forest(total_score)
+    
+    elif main_scenario == 2:
+        print_pause("While exploring the forest, you come across a hidden waterfall.")
+        print_pause("Do you want to take a closer look? (1) Yes (2) No")
+        choice = input()
+        if choice == '1':
+            outcome = random.choice([1, 2, 3])
+            if outcome == 1:
+                print_pause("As you approach the waterfall, you discover a hidden cave behind it.")
+                print_pause("Inside the cave, you find ancient artifacts.")
+                print_pause("Your score increases as you uncover valuable relics.")
+                total_score += 8
+            elif outcome == 2:
+                print_pause("You step closer to the waterfall and slip on a wet rock.")
+                print_pause("You fall into the water but manage to swim back to safety.")
+                print_pause("Your score remains unchanged.")
+            else:
+                print_pause("You admire the beauty of the waterfall and feel refreshed.")
+                print_pause("The serene atmosphere increases your energy.")
+                print_pause("Your score slightly increases.")
+                total_score += 3
+        elif choice == '2':
+            print_pause("You decide not to explore the waterfall and continue your journey.")
+        else:
+            print_pause("Invalid input. Please enter 1 or 2.")
+            return explore_forest(total_score)
+    
     else:
-        print_pause("Invalid input. Please enter 1 or 2.")
-        return explore_forest(total_score)
+        print_pause("As you walk deeper into the forest, you notice a peculiar stone circle.")
+        print_pause("Do you want to investigate? (1) Yes (2) No")
+        choice = input()
+        if choice == '1':
+            outcome = random.choice([1, 2, 3])
+            if outcome == 1:
+                print_pause("You examine the stone circle and discover hidden markings.")
+                print_pause("Deciphering the markings reveals a secret map.")
+                print_pause("Your score increases as you gain valuable knowledge.")
+                total_score += 5
+            elif outcome == 2:
+                print_pause("As you approach the stone circle, a swarm of bees emerges.")
+                print_pause("You quickly retreat to avoid their stings.")
+                print_pause("Your score remains unchanged.")
+            else:
+                print_pause("You sit in the middle of the stone circle and meditate.")
+                print_pause("The tranquil atmosphere replenishes your spirit.")
+                print_pause("Your score slightly increases.")
+                total_score += 2
+        elif choice == '2':
+            print_pause("You decide not to investigate the stone circle and continue your journey.")
+        else:
+            print_pause("Invalid input. Please enter 1 or 2.")
+            return explore_forest(total_score)
+    
     return total_score
 
 def encounter_enemy(total_score):
@@ -34,14 +102,31 @@ def encounter_enemy(total_score):
     print_pause("Do you want to fight? (1) Yes (2) No")
     choice = input()
     if choice == '1':
-        outcome = random.choice(['win', 'lose'])
-        if outcome == 'win':
-            print_pause("You defeat the enemy and continue your journey.")
+        outcome = random.choice([1, 2, 3, 4, 5, 6])
+        if outcome == 1:
+            print_pause("You summon all your courage and engage in a fierce battle.")
+            print_pause("With your exceptional skills, you defeat the enemy!")
             total_score += 5
+        elif outcome == 2:
+            print_pause("The enemy proves to be too strong for you.")
+            print_pause("You barely escape with your life intact.")
+            total_score -= 3
+        elif outcome == 3:
+            print_pause("You manage to land a few good hits on the enemy.")
+            print_pause("Injured, the enemy retreats, allowing you to continue your journey.")
+            total_score += 2
+        elif outcome == 4:
+            print_pause("You find a hidden shortcut during the battle.")
+            print_pause("You make a swift escape, leaving the enemy behind.")
+            total_score += 3
+        elif outcome == 5:
+            print_pause("You cleverly trick the enemy, gaining the upper hand in the fight.")
+            print_pause("Confused, the enemy surrenders and flees the scene.")
+            total_score += 4
         else:
-            print_pause("The enemy overwhelms you. You lose the battle.")
-            print_pause("Game over!")
-            total_score -= 5
+            print_pause("You engage in an epic battle but end up in a stalemate.")
+            print_pause("Both you and the enemy retreat, preparing for future encounters.")
+            total_score += 1
     elif choice == '2':
         print_pause("You choose not to fight and try to escape.")
     else:
@@ -49,15 +134,52 @@ def encounter_enemy(total_score):
         return encounter_enemy(total_score)
     return total_score
 
+
 def meet_stranger(total_score):
     print_pause("While exploring the forest, you come across a friendly stranger.")
     print_pause("The stranger offers you a valuable item.")
     print_pause("Do you accept? (1) Yes (2) No")
     choice = input()
     if choice == '1':
-        item = random.choice(['Sword', 'Shield', 'Potion'])
-        print_pause(f"You graciously accept the {item} from the stranger.")
-        total_score += 3
+        outcome = random.choice([1, 2, 3, 4, 5, 6])
+        if outcome == 1:
+            item = 'Sword'
+            print_pause(f"You graciously accept the {item} from the stranger.")
+            total_score += 3
+        elif outcome == 2:
+            item = 'Shield'
+            print_pause(f"You graciously accept the {item} from the stranger.")
+            total_score += 3
+        elif outcome == 3:
+            item = 'Potion'
+            print_pause(f"You graciously accept the {item} from the stranger.")
+            total_score += 3
+        elif outcome == 4:
+            print_pause("The stranger gives you an old map with hidden treasures marked.")
+            print_pause("You gain valuable information.")
+            total_score += 5
+        elif outcome == 5:
+            print_pause("The stranger challenges you to a friendly game of riddles.")
+            print_pause("If you answer correctly, you'll be rewarded.")
+            answer = input("What has a heart that doesn't beat? ")
+            if answer.lower() == "artichoke":
+                print_pause("Congratulations! You answered correctly.")
+                print_pause("The stranger rewards you with a rare artifact.")
+                total_score += 7
+            else:
+                print_pause("Oops! That's incorrect.")
+                print_pause("The stranger leaves disappointed, and you gain no score.")
+        else:
+            print_pause("The stranger turns out to be a disguised enemy!")
+            print_pause("You engage in a battle to defend yourself.")
+            outcome = random.choice(['win', 'lose'])
+            if outcome == 'win':
+                print_pause("You defeat the enemy and continue your journey.")
+                total_score += 5
+            else:
+                print_pause("The enemy overwhelms you. You lose the battle.")
+                print_pause("Game over!")
+                total_score -= 5
     elif choice == '2':
         print_pause("You politely decline the stranger's offer.")
     else:
@@ -83,12 +205,27 @@ def play_game():
         else:
             print_pause("Invalid input. Please enter 1, 2, or 3.")
 
-        if total_score >= 20:
-            print_pause("Congratulations! You have achieved a high score.")
+        if total_score >= 10:
+            win_sentences = [
+                "Congratulations! You have achieved a high score.",
+                "You have conquered the forest and found the ultimate treasure!",
+                "With bravery and wit, you emerge victorious!",
+                "You are a true adventurer! Well done!",
+                "The forest bows down to your greatness. You win!"
+                ]
+            print_pause(random.choice(win_sentences))
             print_pause("You win!")
             break
-        elif total_score <= -10:
-            print_pause("Your score is too low. Game over!")
+
+        elif total_score <= -5:
+            lose_sentences = [
+                "Your score is too low. Game over!",
+                "Defeat engulfs you. The forest claims another victim.",
+                "You couldn't withstand the perils of the forest. Game over!",
+                "Your journey ends here. The forest remains unconquered.",
+                "The darkness consumes you. Game over!"
+                ]
+            print_pause(random.choice(lose_sentences))
             break
 
     print_pause("Thank you for playing. Goodbye!")
